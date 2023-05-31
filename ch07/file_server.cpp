2e8c107deddd51355dbbca0c6b4fd1f2e29f3a8d
rurouni <53394846+Rurouni-z@users.noreply.github.com>
2023-05-31 20:46:18 +0800

    Update and rename file_server.c to file_server.cpp

--- ch07/file_server.cpp
+++ ch07/file_server.cpp
@@ -6,26 +6,20 @@
 #include <sys/socket.h>
 
 #define BUF_SIZE 30
-void error_handling(char *message);
+void error_handling(const char *message);
 
 int main(int argc, char *argv[])
 {
-    int serv_sd, clnt_sd;
-    FILE *fp;
-    char buf[BUF_SIZE];
-    int read_cnt;
-
-    struct sockaddr_in serv_adr, clnt_adr;
-    socklen_t clnt_adr_sz;
-
     if (argc != 2)
     {
         printf("Usage : %s <port>\n", argv[0]);
         exit(1);
     }
-    fp = fopen("file_server.c", "rb");
-    serv_sd = socket(PF_INET, SOCK_STREAM, 0);
+    
+    FILE *fp = fopen("file_server.cpp", "rb");
+    int serv_sd = socket(PF_INET, SOCK_STREAM, 0);
 
+    struct sockaddr_in serv_adr;
     memset(&serv_adr, 0, sizeof(serv_adr));
     serv_adr.sin_family = AF_INET;
     serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
@@ -34,9 +28,11 @@ int main(int argc, char *argv[])
     bind(serv_sd, (struct sockaddr *)&serv_adr, sizeof(serv_adr));
     listen(serv_sd, 5);
 
-    clnt_adr_sz = sizeof(clnt_adr);
-    clnt_sd = accept(serv_sd, (struct sockaddr *)&clnt_adr, &clnt_adr_sz);
+    struct sockaddr_in clnt_adr;
+    int clnt_sd = accept(serv_sd, (struct sockaddr *)&clnt_adr, (socklen_t*)sizeof(clnt_adr));
 
+    char buf[BUF_SIZE];
+    int read_cnt;
     while (1)
     {
         //从文件流中读取数据，buffer为接收数据的地址，size为一个单元的大小，count为单元个数，stream为文件流
@@ -60,7 +56,7 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-void error_handling(char *message)
+void error_handling(const char *message)
 {
     fputs(message, stderr);
     fputc('\n', stderr);
