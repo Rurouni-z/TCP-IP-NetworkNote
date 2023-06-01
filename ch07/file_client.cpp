43bde1255e1f4558f7005f7309771242ea88f9b0
rurouni <53394846+Rurouni-z@users.noreply.github.com>
2023-06-01 19:05:27 +0800

    Update file_client.cpp

--- ch07/file_client.cpp
+++ ch07/file_client.cpp
@@ -18,22 +18,25 @@ int main(int argc, char *argv[])
     
     FILE *fp;
     int sd;
-    fp = fopen("receive.cpp", "wb");
+    fp = fopen("receive.txt", "wb");
     sd = socket(PF_INET, SOCK_STREAM, 0);
     
     struct sockaddr_in serv_adr;
     memset(&serv_adr, 0, sizeof(serv_adr));
     serv_adr.sin_family = AF_INET;
-    serv_adr.sin_addr.s_addr = inet_addr(argv[1]);
-    serv_adr.sin_port = htons(atoi(argv[2]));
+//     serv_adr.sin_addr.s_addr = inet_addr(argv[1]);
+//     serv_adr.sin_port = htons(atoi(argv[2]));
+    serv_adr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    serv_adr.sin_port = htons(8888);
 
     connect(sd, (struct sockaddr *)&serv_adr, sizeof(serv_adr));
 
     char buf[BUF_SIZE];
     int read_cnt;
-    while ((read_cnt = read(sd, buf, BUF_SIZE)) != 0)
+    while ((read_cnt = read(sd, buf, BUF_SIZE)) != 0){
+        sleep(1);  // 防止接收到垃圾数据，而爆内存，建议输出一下buf
         fwrite((void *)buf, 1, read_cnt, fp);
-
+    }
     puts("Received file data");
     write(sd, "Thank you", 10);
     fclose(fp);
