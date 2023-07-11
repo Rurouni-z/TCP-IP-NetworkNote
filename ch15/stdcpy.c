01f3548ae60eff5a71add8f66c8fef53ae7f86dc
rurouni <53394846+Rurouni-z@users.noreply.github.com>
2023-07-11 20:32:08 +0800

    Update stdcpy.c

--- ch15/stdcpy.c
+++ ch15/stdcpy.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <sys/io.h>
+#include <unistd.h>
 #define BUF_SZIE 3
 
 int main(int argc, char *argv[])
@@ -8,8 +8,8 @@ int main(int argc, char *argv[])
     FILE *fp2;
     char buf[BUF_SZIE];
 
-    fp1 = open("news.txt", "r");
-    fp2 = open("cpy.txt", "w");
+    fp1 = fopen("news.txt", "r");
+    fp2 = fopen("cpy.txt", "w");
 
     while (fgets(buf, BUF_SZIE, fp1) != NULL)
         fputs(buf, fp2);
